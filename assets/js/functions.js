754f1725b4f02ab6b7b83467c146819c9ed9ea84
farazhashmi21 <farazhashmi9000@hotmail.com>
2023-08-31 18:16:30 +0000

    New Js Function Added

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,3 +2,13 @@ function displ(){
     let greetings = "Assalam-O-Alaiqum World";
     console.log(greetings);
 }
+function mouseMove(){
+// Attach an event listener to the "mousemove" event
+    document.addEventListener('mousemove', function() {
+    // Display a prompt when the mouse is moved
+        var userInput = prompt('Hey there! Move your mouse to see this prompt.');
+        // window.location.assign("https://www.hashmiwebsol.com/");
+        // Display the user's input in the console
+        console.log('User input:', userInput);
+    });
+}
